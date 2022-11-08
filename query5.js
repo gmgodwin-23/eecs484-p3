2a826ef1c16345d8b7012bdf7c2490f62692a467
gmgodwin-23 <gmgodwin@umich.edu>
2022-11-07 21:33:00 -0500

    query 5 in progress

--- query5.js
+++ query5.js
@@ -13,5 +13,76 @@ function oldest_friend(dbname) {
     let results = {};
     // TODO: implement oldest friends
 
+    // query 2
+    db.users.aggregate([ 
+        {
+            $unwind : "$friends"
+        },
+        {
+            $group : {
+                _id: "$user_id"
+            }
+        },
+        // { 
+        //     $project : {
+        //         _id : 0,
+        //         user_id : 1,  
+        //         friends : 1
+        //     }
+        // },
+        {
+            $out : "sorted_friends"
+        }
+    ]);
+    
+    db.sorted_friends.find({ }).sort( 
+        {
+            YOB : 1,
+            user_id : 1 
+        } 
+    );
+
+
+    db.sorted_friends.find().forEach(function() {
+        results.push([user_id, friends]);
+    });
+
+
+    //
+    db.users.find().forEach(function(userA) {
+        // search for other friends (cases where friend user_id is )
+        db.users.find().forEach(function(userB) {
+            let all_friends = [];
+            db.users.find([
+                {
+                    friends : {
+                        $in : [userA]
+                    }
+                },
+                {
+                    _id : 0,
+                    user_id : 1
+                }
+            ])
+        })
+    });
+
+    // ***************
+    db.users.find().forEach(function(userA) {
+        db.users.find([
+            {
+                friends : {
+                    $size : { $gt : 0 }, 
+                    $in : [ 597 ]
+                }
+            },
+            {
+                _id : 0,
+                user_id : 1,
+                friends : 1
+            }
+        ])
+    });
+
     return results;
 }
