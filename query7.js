9f8c928cf373188d33fb56302020fac6db62533f
gmgodwin-23 <gmgodwin@umich.edu>
2022-11-08 19:19:42 -0500

    queries 7 + 8

--- query7.js
+++ query7.js
@@ -3,15 +3,25 @@
 
 let num_month_mapper = function () {
     // TODO: Implement the map function
+    emit(this.MOB, this.user_id);
 };
 
-let num_month_reducer = function (key, values) {
+let num_month_reducer = function (keyMOB, userIds) {
     // TODO: Implement the reduce function
+    return userIds.length;
 };
 
-let num_month_finalizer = function (key, reduceVal) {
+let num_month_finalizer = function (keyMOB, numUserIds) {
     // We've implemented a simple forwarding finalize function. This implementation
     // is naive: it just forwards the reduceVal to the output collection.
     // TODO: Feel free to change it if needed.
-    return reduceVal;
+    return numUserIds;
 };
+
+db.users.mapReduce( num_month_mapper,
+    num_month_reducer,
+    {
+      out: "born_each_month",
+      finalize: num_month_finalizer
+    }
+  );
--- query8.js
+++ query8.js
@@ -3,15 +3,36 @@
 
 let city_average_friendcount_mapper = function () {
     // TODO: Implement the map function
+    var valueTuple = {count: 1, numFriends: this.friends.length}
+    emit(this.hometown.city, valueTuple);
 };
 
-let city_average_friendcount_reducer = function (key, values) {
+let city_average_friendcount_reducer = function (hometownCity, valueTuple) {
     // TODO: Implement the reduce function
+    var reducedVal = {totalUsers: 0, totalFriends: 0};
+
+    for (var i = 0; i < valueTuple.length; i++) {
+        reducedVal.totalUsers += valueTuple[i].count;
+        reducedVal.totalFriends += valueTuple[i].numFriends;
+    }
+
+    return reducedVal;
 };
 
-let city_average_friendcount_finalizer = function (key, reduceVal) {
+let city_average_friendcount_finalizer = function (hometownCity, reducedVal) {
     // We've implemented a simple forwarding finalize function. This implementation
     // is naive: it just forwards the reduceVal to the output collection.
     // TODO: Feel free to change it if needed.
-    return reduceVal;
+    
+    var avg = reducedVal.totalFriends / reducedVal.totalUsers;
+
+    return avg;
 };
+
+db.users.mapReduce( city_average_friendcount_mapper,
+    city_average_friendcount_reducer,
+    {
+      out: { merge: "friend_city_population" },
+      finalize: city_average_friendcount_finalizer
+    }
+  )
